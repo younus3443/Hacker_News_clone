44c3319c951f358a7254fb19e0f03ba3e0d5c34d
Syed Younus <162988219+younus3443@users.noreply.github.com>
2025-12-31 10:31:51 +0530

    Switch to dj_database_url for database settings
    Updated database configuration to use dj_database_url for production settings.

--- Hackernews/Hackernews/settings.py
+++ Hackernews/Hackernews/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'Hackernews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
